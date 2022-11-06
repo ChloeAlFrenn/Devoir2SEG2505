fe25aeb5a48ed4e752305096a79c19131b92fb1a
Chloe Al-Frenn <chloealfrenn@hotmail.com>
2022-11-05 22:27:56 -0400

    Exercice 1a Overrides the methods connectionClosed: prints the message that the connection has been closed using the method display.
    Overrides the methods connectionException:
    prints the message that the server stopped using the method display. and
    exits.

--- Assignment2/simpleChat/client/ChatClient.java
+++ Assignment2/simpleChat/client/ChatClient.java
@@ -57,7 +57,34 @@ public class ChatClient extends AbstractClient
   public void handleMessageFromServer(Object msg) 
   {
     clientUI.display(msg.toString());
+ 
   }
+  
+  /**
+	 * Implementation of the hook method called after the connection has been closed. The default
+	 * implementation does nothing. The method may be overriden by subclasses to
+	 * perform special processing such as cleaning up and terminating, or
+	 * attempting to reconnect.
+	 */
+  	@Override
+	public void connectionClosed() {
+  		clientUI.display("the conneciton has been closed");
+	}
+
+	/**
+	 * Implementation of the hook method called each time an exception is thrown by the client's
+	 * thread that is waiting for messages from the server. The method may be
+	 * overridden by subclasses.
+	 * 
+	 * @param exception
+	 *            the exception raised.
+	 */
+  	@Override
+	protected void connectionException(Exception exception) {
+  		clientUI.display("the server stopped");
+  		System.exit(0);
+	}
+	
 
   /**
    * This method handles all data coming from the UI            
